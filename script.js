2eb4d1990167a43546976bb824b6e7572ffe62d6
Vysakh Gireesh <vysakhgireesh07@gmail.com>
2024-08-01 12:32:15 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function changeLogo(){
+    document.getElementById("logo").setAttribute("src","./images/Google-Logo.png")
+}
+
+function reset(){
+    document.getElementById("logo").setAttribute("src","./images/Entri-Logo.png")
+}
